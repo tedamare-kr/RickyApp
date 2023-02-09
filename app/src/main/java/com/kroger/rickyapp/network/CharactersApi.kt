6fadf755f81c79edd9d57c2648a74e6bfc05b8e5
ta36890 <tewodros.amare@kroger.com>
2023-02-09 00:37:42 -0500

    give charactersAPI query

--- app/src/main/java/com/kroger/rickyapp/network/CharactersApi.kt
+++ app/src/main/java/com/kroger/rickyapp/network/CharactersApi.kt
@@ -1,10 +1,14 @@
 package com.kroger.rickyapp.network
 
 import com.kroger.rickyapp.models.CharacterResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CharactersApi {
     @GET("character")
-    fun getAllCharacters(): Call<CharacterResponse>
+    suspend fun getAllCharacters(
+        @Query("page")
+        pageNumber: Int = 1
+    ): Response<CharacterResponse>
 }
