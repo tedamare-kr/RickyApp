6e126ec3a5a50f64d47c1f46fb19d04819ad49f4
ta36890 <tewodros.amare@kroger.com>
2023-02-24 16:13:11 -0500

    refactor api service to dagger network module

--- app/src/main/java/com/kroger/rickyapp/network/RickAndMortyService.kt
+++ app/src/main/java/com/kroger/rickyapp/network/RickAndMortyService.kt
@@ -6,7 +6,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CharactersApi {
+interface RickAndMortyService {
     @GET("character")
     suspend fun getAllCharacters(
         @Query("page")
