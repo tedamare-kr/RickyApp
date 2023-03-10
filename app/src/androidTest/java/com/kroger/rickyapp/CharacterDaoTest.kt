c30027f48e8a2dad088e7e45e5377e09b8c1ca77
ta36890 <tewodros.amare@kroger.com>
2023-03-10 17:05:12 -0500

    test character dao class

--- app/src/androidTest/java/com/kroger/rickyapp/CharacterDaoTest.kt
+++ app/src/androidTest/java/com/kroger/rickyapp/CharacterDaoTest.kt
@@ -44,27 +44,50 @@ class CharacterDaoTest : TestCase() {
     }
 
     @Test
-    fun createAndReadCharacter(): Unit = runBlocking {
-        val character = Character(
+    fun testUpsertCharacter(): Unit = runBlocking {
+        val character = Character(id = 1, name = "Rick Sanchez", status = "Alive", species = "Human", type = "Genius", gender = "Male", image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg", url = "https://rickandmortyapi.com/api/character/1", created = "2017-11-04T18:48:46.250Z")
+
+        characterDao.upsertCharacter(character)
+
+        val updatedCharacter = Character(
             id = 1,
-            name = "Ricky Sanchez",
-            status = "Alive",
+            name = "Teddy Amare",
+            status = "Alive and Well",
             species = "Human",
-            type = "na",
+            type = "",
             gender = "Male",
-            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-            url = "https://rickandmortyapi.com/api/location/3",
-            created = "2017-11-04T18:48:46.250Z"
+            image = "none",
+            url = "none",
+            created = "July 18, 2001"
         )
 
-        characterDao.insertCharacter(character)
+        characterDao.upsertCharacter(updatedCharacter)
 
-        val response = characterDao.getAllCharacterByID(1)
-        assertThat(response).isEqualTo(character)
+        val response = characterDao.getAllCharacters()
+        assertThat(response).contains(updatedCharacter)
+        assertThat(response).doesNotContain(character)
+        assertThat(response).hasSize(1)
     }
 
     @Test
-    fun deleteCharacter(): Unit = runBlocking {
+    fun testInsertAllCharacters(): Unit = runBlocking {
+        val characters = listOf(
+            Character(id = 1, name = "Rick Sanchez", status = "Alive", species = "Human", type = "Genius", gender = "Male", image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg", url = "https://rickandmortyapi.com/api/character/1", created = "2017-11-04T18:48:46.250Z"),
+            Character(id = 2, name = "Morty Smith", status = "Alive", species = "Human", type = "", gender = "Male", image = "https://rickandmortyapi.com/api/character/avatar/2.jpeg", url = "https://rickandmortyapi.com/api/character/2", created = "2017-11-04T18:50:21.651Z"),
+            Character(id = 3, name = "Summer Smith", status = "Alive", species = "Human", type = "", gender = "Female", image = "https://rickandmortyapi.com/api/character/avatar/3.jpeg", url = "https://rickandmortyapi.com/api/character/3", created = "2017-11-04T19:09:56.428Z"),
+            Character(id = 4, name = "Jerry Smith", status = "Alive", species = "Human", type = "", gender = "Male", image = "https://rickandmortyapi.com/api/character/avatar/4.jpeg", url = "https://rickandmortyapi.com/api/character/4", created = "2017-11-04T19:22:43.665Z"),
+            Character(id = 5, name = "Beth Smith", status = "Alive", species = "Human", type = "", gender = "Female", image = "https://rickandmortyapi.com/api/character/avatar/5.jpeg", url = "https://rickandmortyapi.com/api/character/5", created = "2017-11-04T19:26:56.301Z"),
+            Character(id = 7, name = "Birdperson", status = "Dead", species = "Alien", type = "Bird-Person", gender = "Male", image = "https://rickandmortyapi.com/api/character/avatar/12.jpeg", url = "https://rickandmortyapi.com/api/character/12", created = "2017-11-04T20:03:34.737Z")
+        )
+
+        characterDao.insertAllCharacters(characters)
+
+        val result = characterDao.getAllCharacters()
+        assertThat(result).isEqualTo(characters.sortedBy { it.name })
+    }
+
+    @Test
+    fun testGetAllCharacters(): Unit = runBlocking {
         val character = Character(
             id = 1,
             name = "Ricky Sanchez",
@@ -77,9 +100,30 @@ class CharacterDaoTest : TestCase() {
             created = "2017-11-04T18:48:46.250Z"
         )
 
-        characterDao.insertCharacter(character)
+        characterDao.upsertCharacter(character)
 
-        val response = characterDao.deleteCharacter(character)
-        assertThat(response).isEqualTo(character)
+        val response = characterDao.getAllCharacters()
+        assertThat(response).contains(character)
+        assertThat(response).hasSize(1)
+    }
+
+    @Test
+    fun testDeleteCharacter(): Unit = runBlocking {
+        val character = Character(
+            id = 1,
+            name = "Ricky Sanchez",
+            status = "Alive",
+            species = "Human",
+            type = "na",
+            gender = "Male",
+            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            url = "https://rickandmortyapi.com/api/location/3",
+            created = "2017-11-04T18:48:46.250Z"
+        )
+        characterDao.upsertCharacter(character)
+        characterDao.deleteCharacter(character)
+
+        val responseList = characterDao.getAllCharacters()
+        assertThat(responseList).doesNotContain(character)
     }
 }
