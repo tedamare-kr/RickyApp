9b76e1d6e9f11e071a4abdf934ca2f94ecb18328
ta36890 <tewodros.amare@kroger.com>
2023-03-17 15:53:12 -0400

    inject dao instead of database

--- app/src/main/java/com/kroger/rickyapp/ui/characters/CharactersRepository.kt
+++ app/src/main/java/com/kroger/rickyapp/ui/characters/CharactersRepository.kt
@@ -1,26 +1,26 @@
 package com.kroger.rickyapp.ui.characters
 
-import android.util.Log
-import com.kroger.rickyapp.db.CharacterDatabase
+import com.kroger.rickyapp.db.CharacterDao
 import com.kroger.rickyapp.models.Character
 import com.kroger.rickyapp.models.CharacterResponse
 import com.kroger.rickyapp.network.RickAndMortyService
+import com.kroger.rickyapp.util.TimeUtils
 import javax.inject.Inject
 
 class CharactersRepository @Inject constructor(
     private val rickAndMortyService: RickAndMortyService,
-    private val characterDatabase: CharacterDatabase
+    private val characterDao: CharacterDao
 ) {
 
     private var lastAPIRequestTime: Long = 0
     private val FIVE_MINUTES_IN_MILLIS: Long = 300_000
 
     suspend fun getTheChars(): CharacterUIData {
-        val currentTime = System.currentTimeMillis()
+        val currentTime = TimeUtils.getCurrentTimeMillis()
         val elapsedTime = currentTime.minus(lastAPIRequestTime)
 
         // check if it has been more than 5 min
-        Log.i("Current Time: ", "$currentTime vs $elapsedTime")
+//        Log.i("Current Time: ", "$currentTime vs $elapsedTime")
         return if (elapsedTime > FIVE_MINUTES_IN_MILLIS) {
             // Make API request and update the database
             val response = getAllCharactersFromAPI(pageNumber = 1)
@@ -40,9 +40,9 @@ class CharactersRepository @Inject constructor(
         rickAndMortyService.getAllCharacters(pageNumber)
 
     private fun getCharsFromDB(): List<Character> =
-        characterDatabase.characterDao().getAllCharacters()
+        characterDao.getAllCharacters()
 
     private suspend fun insertAllCharacters(characters: List<Character>) {
-        characterDatabase.characterDao().insertAllCharacters(characters)
+        characterDao.insertAllCharacters(characters)
     }
 }
